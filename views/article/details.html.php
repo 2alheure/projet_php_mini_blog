86fb6b41d556be0a0209833374d48d53f2f1f2d1
2alheure <jojo77.juv@live.fr>
2021-05-28 13:10:16 +0200

    Fix buttons dans détails

--- views/article/details.html.php
+++ views/article/details.html.php
@@ -6,6 +6,11 @@
 </div>
 <small>Publié le <?php echo date_formatee($article->date_de_publication); ?> par <?php echo $article->auteur; ?></small>
 
+<p>
+	<a href="<?php echo BASE_URL . '/modifier-article?id=' . $article->id; ?>" class="btn btn-primary">Modifier</a>
+	<a href="<?php echo BASE_URL . '/supprimer-article?id=' . $article->id; ?>" class="btn btn-danger">Supprimer</a>
+</p>
+
 <h1 class="mb-4"><?php echo $article->titre; ?></h1>
 
 <p><?php echo $article->contenu; ?></p>

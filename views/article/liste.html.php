9f3ff07c87debd6aae7649aa5bcfdc6709d30386
2alheure <jojo77.juv@live.fr>
2021-05-28 15:24:26 +0200

    14 - N'afficher les liens qu'à l'admin

--- views/article/liste.html.php
+++ views/article/liste.html.php
@@ -12,7 +12,7 @@
 			</div>
 			<p class="mb-1"><?php echo resume($article); ?></p>
 			<small class="text-muted"><a href="<?php echo BASE_URL . '/details-article?id=' . $article->id; ?>">Lire l'article.</a></small>
-			<?php if (isset($_SESSION['role']) && $_SESSION['role'] == 'admin') { ?>
+			<?php if (isset($_SESSION['utilisateur']->role) && $_SESSION['utilisateur']->role == 'admin') { ?>
 				<p class="m-0 mt-3">
 					<a href="<?php echo BASE_URL . '/modifier-article?id=' . $article->id; ?>" class="btn btn-primary">Modifier</a>
 					<a href="<?php echo BASE_URL . '/supprimer-article?id=' . $article->id; ?>" class="btn btn-danger">Supprimer</a>

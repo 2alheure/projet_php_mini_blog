76511377340744ab001f2868b818aa25d3cea6a3
2alheure <jojo77.juv@live.fr>
2021-05-28 13:09:38 +0200

    Fix button dans détails

--- views/article/details.html.php
+++ views/article/details.html.php
@@ -6,6 +6,10 @@
 </div>
 <small>Publié le <?php echo date_formatee($article->date_de_publication); ?> par <?php echo $article->auteur; ?></small>
 
+<p>
+	<a href="<?php echo BASE_URL . '/modifier-article?id=' . $article->id; ?>" class="btn btn-primary">Modifier</a>
+</p>
+
 <h1 class="mb-4"><?php echo $article->titre; ?></h1>
 
 <p><?php echo $article->contenu; ?></p>
